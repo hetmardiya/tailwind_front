191408d1f43b54e1786ea6b6248a52f28e503961
Het Mardiya <hetmardiya9@gmail.com>
2024-08-16 01:50:19 +0530

    add a dynamic path with express and hbs

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const path = require('path');
+
+const hbsFilePath = path.join(__dirname , "./website/dist")
+const partialFilePath = path.join(__dirname , "./website/components")
+
+const isrcPath = path.join(__dirname , "./website")
+
+
+app.use(express.static(isrcPath))
+
+app.set("view engine" , "hbs")
+app.set("views" , hbsFilePath)
+hbs.registerPartials(partialFilePath)
+
+
+
+
+app.get("/" , (req,res)=>{
+    res.render('index')
+})
+
+app.listen(8000 , ()=>{
+    console.log(`app is running at port no 8000`);
+    
+})
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./website/components/*.{hbs,html,js}",
+    "./website/dist/*.{hbs,html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
