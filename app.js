c54676e4aa917ab7b1dfa3dccdea4e7a5b54bba9
Het Mardiya <hetmardiya9@gmail.com>
2024-08-18 20:59:22 +0530

    complete project

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ hbs.registerPartials(partialFilePath)
 app.get("/" , (req,res)=>{
     res.render('index')
 })
+app.get("/contact",(req,res)=>{
+    res.render('contact')
+})
+app.get("/about",(req,res)=>{
+    res.render('about')
+})
+app.get("/login",(req,res)=>{
+    res.render('login')
+})
+app.get("/signin",(req,res)=>{
+    res.render('signin')
+})
 
 app.listen(8000 , ()=>{
     console.log(`app is running at port no 8000`);
